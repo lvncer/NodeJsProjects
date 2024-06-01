b09c050331ddc348ab705786b4072badfa302551
siwTakahasi <147582192+hellotksan@users.noreply.github.com>
2024-06-01 15:39:13 +0900

    first commit

---
+++ node-app/app.js
@@ -0,0 +1,92 @@
+const http = require("http");
+const fs = require("fs");
+const ejs = require("ejs");
+const url = require("url");
+const qs = require("querystring"); //☆追加
+
+const index_page = fs.readFileSync("./index.ejs", "utf8");
+const other_page = fs.readFileSync("./other.ejs", "utf8");
+const style_css = fs.readFileSync("./style.css", "utf8");
+
+var server = http.createServer(getFromClient);
+
+server.listen(3000);
+console.log("Server start!");
+
+// ここまでメインプログラム==========
+
+var data = { msg: "no message..." };
+
+// createServerの処理
+function getFromClient(request, response) {
+  var url_parts = url.parse(request.url, true); //☆trueに
+
+  switch (url_parts.pathname) {
+    case "/":
+      response_index(request, response); //☆修正
+      break;
+
+    case "/other":
+      response_other(request, response); //☆修正
+      break;
+
+    case "/style.css":
+      response.writeHead(200, { "Content-Type": "text/css" });
+      response.write(style_css);
+      response.end();
+      break;
+
+    default:
+      response.writeHead(200, { "Content-Type": "text/plain" });
+      response.end("no page...");
+      break;
+  }
+}
+
+// ☆indexのアクセス処理
+function response_index(request, response) {
+  // POSTアクセス時の処理
+  if (request.method == "POST") {
+    var body = "";
+
+    // データ受信のイベント処理
+    request.on("data", (data) => {
+      body += data;
+    });
+
+    // データ受信終了のイベント処理
+    request.on("end", () => {
+      data = qs.parse(body); // ☆データのパース
+      write_index(request, response);
+    });
+  } else {
+    write_index(request, response);
+  }
+}
+
+// indexの表示の作成
+function write_index(request, response) {
+  var msg = "※伝言を表示します。";
+  var content = ejs.render(index_page, {
+    title: "Index",
+    content: msg,
+    data: data,
+  });
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.write(content);
+  response.end();
+}
+
+// ☆otherのアクセス処理
+function response_other(request, response) {
+  var msg = "これはOtherページです。";
+  var content = ejs.render(other_page, {
+    title: "Other",
+    content: msg,
+    data: data2,
+    filename: "data_item",
+  });
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.write(content);
+  response.end();
+}
---
+++ readme.md
@@ -0,0 +1,28 @@
+# マルチプロジェクト Node.js リポジトリ
+
+このリポジトリには複数の Node.js プロジェクトが含まれています。各プロジェクトは独立して動作しますが、共通の開発環境を共有します。
+
+## プロジェクト一覧
+
+- **node-app**: 基本的な Node.js アプリケーション
+- **mini_board**: ミニ掲示板アプリケーション
+- **express-app**: Express を使用した Web アプリケーション
+- **ex-gen-app**: Express ジェネレータを使用して作成されたアプリケーション
+
+## 依存関係のインストール
+
+各プロジェクトフォルダ内で依存関係をインストールする必要があります。以下のコマンドをそれぞれのプロジェクトディレクトリ内で実行してください。
+
+```bash
+cd node-app
+npm install
+
+cd ../mini_board
+npm install
+
+cd ../express-app
+npm install
+
+cd ../ex-gen-app
+npm install
+```
